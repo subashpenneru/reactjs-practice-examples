19be42b63572515b63203d4f56aeec3a65ffeb79
Subash Penneru <subashpenneru@gmail.com>
2023-01-22 13:54:45 +0530

    files updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,22 @@
-export * from './comments';
-export * from './todos';
+export const getComments = async ({ pageParam = 1 }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?_page=${pageParam}&_limit=${24}`
+  );
+  const data = await res.json();
+  return {
+    comments: data,
+    nextPage: pageParam + 1,
+  };
+};
+
+export const getTodos = async (page = 1, limit = 24) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${limit}`
+  );
+  return await res.json();
+};
+
+export const getProducts = async () => {
+  const res = await fetch(`https://fakestoreapi.com/products`);
+  return await res.json();
+};
--- src/api/todos.js
+++
@@ -1,6 +0,0 @@
-export const getTodos = async (page = 1, limit = 24) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${limit}`
-  );
-  return res.json();
-};
