8214e378725d248871b0b2c194996f0dc0f6dc5d
Subash Penneru <subashpenneru@gmail.com>
2023-02-28 08:58:08 +0530

    alternate routes declaration

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,50 @@ import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 import ErrorPage from './pages/Error';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
-      <Route index element={<WelcomePage />} />
-      <Route path='/blog' element={<BlogLayout />}>
-        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-        <Route
-          path=':id'
-          element={<PostDetailPage />}
-          loader={postDetailLoader}
-        />
-      </Route>
-      <Route
-        path='/blog/new'
-        element={<NewPostPage />}
-        action={newPostAction}
-      />
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
+//       <Route index element={<WelcomePage />} />
+//       <Route path='/blog' element={<BlogLayout />}>
+//         <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+//         <Route
+//           path=':id'
+//           element={<PostDetailPage />}
+//           loader={postDetailLoader}
+//         />
+//       </Route>
+//       <Route
+//         path='/blog/new'
+//         element={<NewPostPage />}
+//         action={newPostAction}
+//       />
+//     </Route>
+//   )
+// );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <WelcomePage /> },
+      {
+        path: '/blog',
+        element: <BlogLayout />,
+        children: [
+          { index: true, element: <BlogPostsPage />, loader: blogPostsLoader },
+          {
+            path: ':id',
+            element: <PostDetailPage />,
+            loader: postDetailLoader,
+          },
+        ],
+      },
+      { path: '/blog/new', element: <NewPostPage />, action: newPostAction },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
